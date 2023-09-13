9f2d927e3a887faa1bf9965cde38887fa2915c61
Jeffrey Schang <schangj09@gmail.com>
2023-09-13 12:21:04 +0200

    fill in all javadocs and apply reformatting

--- src/org/baylight/circles/CirclesApp.java
+++ src/org/baylight/circles/CirclesApp.java
@@ -1,39 +1,49 @@
 package org.baylight.circles;
+
 import java.awt.*;
 import javax.swing.*;
 
-
+/**
+ * The main App class for running the swing program. It invokes the Swing event
+ * dispatcher thread with the Frame and MetaComponent.
+ *
+ * @author Jeffrey Schang
+ */
 class CirclesApp {
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event-dispatching thread.
-     */
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("CirclesApp");
-        frame.setSize(250,250);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        Container pane = frame.getContentPane();
+  /**
+   * Create the GUI and show it. For thread safety, this method should be
+   * invoked from the event-dispatching thread.
+   */
+  private static void createAndShowGUI() {
+    //Create and set up the window.
+    JFrame frame = new JFrame("CirclesApp");
+    frame.setSize(250, 250);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Add the label.
-        //JLabel label = new JLabel("Start drawing");
-        //pane.add(label);
+    Container pane = frame.getContentPane();
 
-        MetaComponent component = new MetaComponent();
-        pane.add(component, BorderLayout.CENTER);
+    //Add the label.
+    //JLabel label = new JLabel("Start drawing");
+    //pane.add(label);
+    MetaComponent component = new MetaComponent();
+    pane.add(component, BorderLayout.CENTER);
 
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
+    //Display the window.
+    frame.pack();
+    frame.setVisible(true);
+  }
 
-    public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            createAndShowGUI();
-        });
-    }
+  /**
+   * Main method to start up the swing UI.
+   *
+   * @param args arguments from the command line
+   */
+  public static void main(String[] args) {
+    //Schedule a job for the event-dispatching thread:
+    //creating and showing this application's GUI.
+    javax.swing.SwingUtilities.invokeLater(() -> {
+      createAndShowGUI();
+    });
+  }
 }
--- src/org/baylight/circles/MetaComponent.java
+++ src/org/baylight/circles/MetaComponent.java
@@ -8,8 +8,14 @@ import javax.swing.*;
 
 import static java.lang.Math.*;
 
-
+/**
+ * A JPanel component that contains a control panel with several UI controls and
+ * additional space on the screen to draw the Metatron Cubes.
+ *
+ * @author Jeffrey Schang
+ */
 class MetaComponent extends JPanel implements ItemListener, ActionListener, Printable {
+
   private static final Dimension preferredSize = new Dimension(1000, 1000);
   private final JPanel controlPanel;
   private final JCheckBox colorCheckbox;
@@ -19,6 +25,9 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
   private boolean showCircles = Boolean.TRUE;
   private int imageCount = 1;
 
+  /**
+   * Default constructor.
+   */
   public MetaComponent() {
     super(new FlowLayout());
 
@@ -33,9 +42,9 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
     circlesCheckbox.addItemListener(this);
 
     JLabel countLabel = new JLabel("Number of images", JLabel.LEADING);
-    JComboBox<String> countList = new JComboBox<String>(new String[] {"1", "2", "4"});
+    JComboBox<String> countList = new JComboBox<String>(new String[]{"1", "2", "4"});
     countList.addActionListener(this);
-    
+
     printButton = new JButton("Print");
     printButton.setMnemonic(KeyEvent.VK_P);
     printButton.addActionListener(this);
@@ -51,12 +60,19 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
     add(controlPanel);
   }
 
+  /**
+   * Override for the initial preferred size of the screen component.
+   *
+   * @return the preferred size
+   */
   @Override
   public Dimension getPreferredSize() {
     return preferredSize;
   }
 
-  /** Listens to the check boxes. */
+  /**
+   * Listens to the check boxes.
+   */
   @Override
   public void itemStateChanged(ItemEvent e) {
     int index = 0;
@@ -65,7 +81,7 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
 
     if (source == colorCheckbox) {
       colorSelection = (e.getStateChange() == ItemEvent.DESELECTED)
-        ? 'W' : 'B';
+          ? 'W' : 'B';
       setControlPanelColors();
     } else if (source == circlesCheckbox) {
       showCircles = (e.getStateChange() == ItemEvent.SELECTED);
@@ -73,15 +89,17 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
     repaint();
   }
 
-  /** Listens to the drop down combo or print button */
+  /**
+   * Listens to the drop down combo or print button
+   */
   @Override
   public void actionPerformed(ActionEvent e) {
     Object source = e.getSource();
     if (source == printButton) {
       onPrint();
     } else {
-      JComboBox cb = (JComboBox)source;
-      String newSelection = (String)cb.getSelectedItem();
+      JComboBox cb = (JComboBox) source;
+      String newSelection = (String) cb.getSelectedItem();
       try {
         imageCount = Integer.parseInt(newSelection);
       } catch (NumberFormatException exc) {
@@ -91,6 +109,9 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
     }
   }
 
+  /**
+   * Helper method to set the colors of the control panel and its components.
+   */
   protected void setControlPanelColors() {
     Color backgroundColor = (colorSelection == 'B') ? Color.BLACK : Color.WHITE;
     Color foregroundColor = (colorSelection == 'B') ? Color.WHITE : Color.BLACK;
@@ -102,6 +123,10 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
     }
   }
 
+  /**
+   * Helper method to invoke the print dialog. When the user presses Print in
+   * the dialog then the framework will call the print method.
+   */
   protected void onPrint() {
     PrinterJob job = PrinterJob.getPrinterJob();
     job.setPrintable(this);
@@ -116,21 +141,33 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
     }
   }
 
+  /**
+   * Override the default print method to implement printing functionality. We
+   * print only one page with the diagram occupying as much of the page as
+   * possible.
+   *
+   * @param g the printer graphics context
+   * @param pf the page format details
+   * @param page the index of the page to print
+   * @return a code from {@link Printable} indicating whether there more pages
+   * to print
+   * @throws {@link PrinterException}
+   */
   @Override
   public int print(Graphics g, PageFormat pf, int page)
-        throws PrinterException {
+      throws PrinterException {
 
     // We have only one page, and 'page'
     // is zero-based
     if (page > 0) {
-         return NO_SUCH_PAGE;
+      return NO_SUCH_PAGE;
     }
 
     // User (0,0) is typically outside the
     // imageable area, so we must translate
     // by the X and Y values in the PageFormat
     // to avoid clipping.
-    Graphics2D g2d = (Graphics2D)g;
+    Graphics2D g2d = (Graphics2D) g;
     g2d.translate(pf.getImageableX(), pf.getImageableY());
 
     // Stroke dashedStroke = new BasicStroke(
@@ -142,7 +179,7 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
     //   0.0f
     // );
     Stroke thinStroke = new BasicStroke(
-      0.4f
+        0.4f
     );
     g2d.setStroke(thinStroke);
 
@@ -150,25 +187,31 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
     // diameter to be min(pageWidth, pageHight)/5
     double pageWidth = pf.getImageableWidth();
     double pageHeight = pf.getImageableHeight();
-    double diameter = min(pageWidth, pageHeight)/5;
+    double diameter = min(pageWidth, pageHeight) / 5;
     DrawingContext drawing = new DrawingContext(g2d, diameter, showCircles);
-    Point2D centerPt = new Point2D.Double(pageWidth/2, pageHeight/2);
+    Point2D centerPt = new Point2D.Double(pageWidth / 2, pageHeight / 2);
     double rotation = PI / 6.0;
     double angleIncrement = toRadians(15);
     int indexIncrement = getIndexIncrement();
     for (int i = 0; i < 4; i += indexIncrement) {
       drawing.drawMain(centerPt, rotation);
-      rotation += angleIncrement*indexIncrement;
+      rotation += angleIncrement * indexIncrement;
     }
 
     // tell the caller that this page is part
     // of the printed document
     return PAGE_EXISTS;
   }
-  
+
+  /**
+   * Override the default paintComponent for drawing in the screen Graphics
+   * context.
+   *
+   * @param g the screen graphics context
+   */
   @Override
   public void paintComponent(Graphics g) {
-    super.paintComponent(g);       
+    super.paintComponent(g);
 
     Graphics2D g2d = (Graphics2D) g;
     setBackground(colorSelection == 'B' ? Color.BLACK : Color.WHITE);
@@ -178,49 +221,64 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
     double yOffset = controlPanel.getHeight();
     double width = size.getWidth();
     double height = size.getHeight() - yOffset;
-    Point2D centerPt = new Point2D.Double(width/2, yOffset + height/2);
+    Point2D centerPt = new Point2D.Double(width / 2, yOffset + height / 2);
 
-    Color[] blackColors = new Color[] {
+    Color[] blackColors = new Color[]{
       Color.BLUE, Color.RED, Color.YELLOW, Color.GREEN
     };
-    Color[] whiteColors = new Color[] {
+    Color[] whiteColors = new Color[]{
       Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK
     };
     Color[] colors = colorSelection == 'B' ? blackColors : whiteColors;
 
     // Image is appoximately 5 times the diameter, so choose
     // diameter to be min(pageWidth, pageHight)/5.2
-    double diameter = min(width, height)/5.2;
+    double diameter = min(width, height) / 5.2;
     DrawingContext drawing = new DrawingContext(g2d, diameter, showCircles);
     double rotation = PI / 6.0;
     double angleIncrement = toRadians(15);
     int indexIncrement = getIndexIncrement();
     for (int i = 0; i < 4; i += indexIncrement) {
       drawing.setColor(colors[i]);
-      drawing.drawMain(centerPt, rotation + i*angleIncrement);
+      drawing.drawMain(centerPt, rotation + i * angleIncrement);
     }
   }
 
+  /**
+   * Helper method to calculate the value to increment the index based on the
+   * desired number of instances of the metatron cube.
+   *
+   * @return the increment value
+   */
   protected int getIndexIncrement() {
-      return switch (imageCount) {
-          case 1 -> 4;
-          case 2 -> 2;
-          default -> 1;
-      };
+    return switch (imageCount) {
+      case 1 ->
+        4;
+      case 2 ->
+        2;
+      default ->
+        1;
+    };
   }
 
   /**
-   * Inner class for drawing the circles into a given graphics context:
-   *   Graphics context.
-   *   Diameter of the circles.
-   *   Whether to show the circles or just the connecting lines.
-  **/
+   * Inner class for drawing a metatron cube into a given {@link Graphics2D}.
+   * The diameter is fixed but the rotation is specified for each call.
+   */
   protected class DrawingContext {
+
     private static final double deg60 = PI / 3.0;
     private final Graphics2D g2d;
     private final double diameter;
     private final boolean showCircles;
 
+    /**
+     * Default constructor.
+     *
+     * @param g2d the graphics context to draw into
+     * @param diameter the diameter of the circles in the metatron cube
+     * @param showCircles whether to show the circles in addition to lines
+     */
     DrawingContext(Graphics2D g2d, double diameter, boolean showCircles) {
       this.g2d = g2d;
       this.diameter = diameter;
@@ -232,16 +290,19 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
     }
 
     /**
-     * Draw all circles and connecting lines of the metatron with an initial rotation
-     * offset.
-     * 
+     * Draw all circles and connecting lines of the metatron with an initial
+     * rotation offset.
+     *
      * Note: if showCircles is false, then only the connecting lines are drawn.
-     **/
+     *
+     * @param centerPt the center of the metatron cube
+     * @param rotation the rotation offset to use
+     */
     public void drawMain(Point2D centerPt, double rotation) {
       drawCircle(centerPt);
       double dist = diameter;
       Point2D[] innerPts = draw6(centerPt, dist, rotation);
-      Point2D[] outerPts = draw6(centerPt, dist*2, rotation);
+      Point2D[] outerPts = draw6(centerPt, dist * 2, rotation);
 
       connectInnerOuter(0, innerPts, outerPts);
       connectInnerOuter(1, innerPts, outerPts);
@@ -252,25 +313,36 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
     }
 
     /**
-     * Draw 2 lines connecting the given outer point to the 2 inner points across
-     * from it that are not already connected by other outer point connecting lines.
-     **/
+     * Draw 2 lines connecting the given outer point to the 2 inner points
+     * across from it that are not already connected by other outer point
+     * connecting lines.
+     *
+     * @param index index of the outer point we are connecting
+     * @param innerPts array of the center points of the inner circles
+     * @param outerPts array of the center points of the outer circles
+     */
     protected void connectInnerOuter(int index, Point2D[] innerPts, Point2D[] outerPts) {
-      drawLine(outerPts[index], innerPts[(index + 2)%6]);
-      drawLine(outerPts[index], innerPts[(index + 4)%6]);
+      drawLine(outerPts[index], innerPts[(index + 2) % 6]);
+      drawLine(outerPts[index], innerPts[(index + 4) % 6]);
     }
 
     /**
-     * Draw 6 circles around the center point with the given distance and an initial
-     * rotation offset. Also connect all of the circle center points by lines.
-     * 
+     * Draw 6 circles around the center point with the given distance and an
+     * initial rotation offset. Also connect all of the circle center points by
+     * lines.
+     *
      * Note: if showCircles is false, then only the connecting lines are drawn.
-     **/
+     *
+     * @param center center of the collection of circles
+     * @param dist distance between the circles center points
+     * @param rotation rotation offset in radians
+     * @return the array of center points of the six circles
+     */
     protected Point2D[] draw6(Point2D center, double dist, double rotation) {
 
       Point2D[] circles = new Point2D[6];
       for (int i = 0; i < 6; i++) {
-        circles[i] = arcPoint(center, dist, rotation + i*deg60);
+        circles[i] = vectorPoint(center, dist, rotation + i * deg60);
         drawCircle(circles[i]);
       }
       Point2D pt1 = circles[0];
@@ -303,6 +375,11 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
       return circles;
     }
 
+    /**
+     * Draw a single circle at the given point.
+     *
+     * @param center the center point
+     */
     protected void drawCircle(Point2D center) {
       if (showCircles) {
         Ellipse2D centerCircle = new Ellipse2D.Double();
@@ -311,18 +388,39 @@ class MetaComponent extends JPanel implements ItemListener, ActionListener, Prin
       }
     }
 
+    /**
+     * Calculate the upper left corner of the rectangle that encloses a circle.
+     *
+     * @param center the center of the circle
+     * @return the upper left corner of the circle
+     */
     protected Point2D corner(Point2D center) {
-      return new Point2D.Double(center.getX() - diameter/2, center.getY() - diameter/2);
+      return new Point2D.Double(center.getX() - diameter / 2, center.getY() - diameter / 2);
     }
 
+    /**
+     * Draw a line between two points.
+     *
+     * @param pt1 the start point
+     * @param pt2 the end point
+     */
     protected void drawLine(Point2D pt1, Point2D pt2) {
       g2d.draw(new Line2D.Double(pt1, pt2));
     }
 
-    protected Point2D arcPoint(Point2D center, double dist, double angle) {
-      double vx = dist*cos(angle);
-      double vy = dist*sin(angle);
-      return new Point2D.Double(center.getX()+vx, center.getY()+vy);
+    /**
+     * Calculate a point with the given vector (distance and angle) from the
+     * center point.
+     *
+     * @param center the center point
+     * @param dist distance of the vector
+     * @param angle angle of the vector in radians
+     * @return the point at the end of the vector
+     */
+    protected Point2D vectorPoint(Point2D center, double dist, double angle) {
+      double vx = dist * cos(angle);
+      double vy = dist * sin(angle);
+      return new Point2D.Double(center.getX() + vx, center.getY() + vy);
     }
   }
 
