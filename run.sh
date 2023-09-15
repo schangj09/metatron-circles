7e5d3cf257a8b9c4c5a8fdeb3887890f1c52d317
Jeffrey Schang <schangj09@gmail.com>
2023-09-15 12:46:31 +0200

    change from netbeans project to maven project

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-javac --source-path src -d class src/org/baylight/circles/*.java && java -cp class org.baylight.circles.CirclesApp 
+java -cp target/classes org.baylight.circles.CirclesApp
